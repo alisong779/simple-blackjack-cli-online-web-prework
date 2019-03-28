abe728d01ee6b0bc1e6a014c8ad1a08bb57cc648
Alison Golden <alisong779@yahoo.com>
2019-03-28 22:46:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,7 +46,8 @@ def runner
   card_total = initial_round
   
   until card_total > 21 do 
-    hit?(card_total)
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   
   end 
   return end_game
